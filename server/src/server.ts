37077dba998ba7dd01d913798d3cfe22503b2c00
vitorhugomarcal <vitorhugomarcal@gmail.com>
2022-11-05 03:42:05 -0300

    initial commit

---
+++ server/src/server.ts
@@ -0,0 +1,36 @@
+import Fastify from "fastify";
+import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
+
+import { poolRoutes } from "./routes/pool";
+import { userRoutes } from "./routes/user";
+import { guessRoutes } from "./routes/guess";
+import { authRoutes } from "./routes/auth";
+import { gameRoutes } from "./routes/game";
+
+async function bootstrap() {
+  const fastify = Fastify({
+      logger: true,
+    })
+
+  await fastify.register(cors, {
+    origin: true,
+  })
+
+  await fastify.register(jwt, {
+    secret: 'teste'
+  })
+
+  await fastify.register(poolRoutes)
+  await fastify.register(authRoutes)
+  await fastify.register(gameRoutes)
+  await fastify.register(guessRoutes)
+  await fastify.register(userRoutes)
+
+  await fastify.listen({
+    port: 3333,
+    host: '0.0.0.0',
+  })
+}
+
+bootstrap()
